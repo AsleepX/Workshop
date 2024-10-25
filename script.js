e29d86ae882005e4084878ed610cc02661cf033f
AsleepX <129823735+AsleepX@users.noreply.github.com>
2024-10-25 21:00:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleDetails(day) {
+    const content = document.getElementById(day);
+    content.style.display = content.style.display === 'none' ? 'block' : 'none';
+}
+
+let lastScrollTop = 0;
+window.addEventListener("scroll", function() {
+    const nav = document.querySelector("nav");
+    nav.classList.add("show");
+});
